d7f4c0910e65a49cc553d2b0c567b37e64429d49
stip <alexander.stippler@ulmvolley.de>
2019-03-22 17:17:13 +0100

    added plotting facility via pipe to gnuplot.

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,19 +1,19 @@
 #include "ioscript/ioscript.h"
 
 void testing();
-void example_python();
+//void example_python();
 void example_gnuplot();
-void example_process();
-void example_readme();
+//void example_process();
+//void example_readme();
 
 int main()
 {
 	testing();
 
-	example_python();
+//	example_python();
 	example_gnuplot();
-	example_process();
-	example_readme();
+//	example_process();
+//	example_readme();
 
 	return 0;
 }
